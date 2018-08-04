f91640fbea5a79fbd8b794726ebfe8f58afc9278
ibhi <ibrahimin2004@gmail.com>
2018-08-04 16:34:56 +0530

    fix: path for acme.json

--- pms.ts
+++ pms.ts
@@ -147,11 +147,11 @@ export DATA_DIRECTORY_PATH=/media
 export CONFIG_DIRECTORY_PATH=/media/config
 export DOWNLOADS_DIRECTORY_PATH=/cache/data/Downloads
 export HOST_NAME=ibhi.tk
-mkdir -p \${CONFIG_DIRECTORY_PATH}/letsencrypt
-chown -R ubuntu:ubuntu \${CONFIG_DIRECTORY_PATH}/letsencrypt
-touch \${CONFIG_DIRECTORY_PATH}/letsencrypt/acme.json
-chown ubuntu:ubuntu \${CONFIG_DIRECTORY_PATH}/letsencrypt/acme.json
-chmod 600 \${CONFIG_DIRECTORY_PATH}/letsencrypt/acme.json
+mkdir -p /media/config/letsencrypt
+chown -R ubuntu:ubuntu /media/config/letsencrypt
+touch /media/config/letsencrypt/acme.json
+chown ubuntu:ubuntu /media/config/letsencrypt/acme.json
+chmod 600 /media/config/letsencrypt/acme.json
 # Docker containers setup
 cd /tmp/pms-aws-cloudformation
 docker network create web
